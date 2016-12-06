4a28f02d7090ffb8ad0388e5464e31e87098ae8f
Takuya Matsumoto <tarqmat@gmail.com>
2016-12-06 13:07:11 +0900

    fix code

--- hello-world.rs
+++ hello-world.rs
@@ -1,7 +1,7 @@
 fn main() {
   let mut num = 108u32;
   println!("Hello world! {:?}", num);
-  // comments
+  // Change number to show
   num = 42;
-  println!("Hello world! {:?}", num);
+  println!("Hello home! {:?}", num);
 }
